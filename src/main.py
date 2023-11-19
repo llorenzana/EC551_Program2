ede436084e0c1e3f1551d663e166e03c70f5f7aa
llorenzana <91199802+llorenzana@users.noreply.github.com>
2023-11-19 14:04:34 -0500

    add choices

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import os
-from blif_to_tt import blif_file_to_tt_file
 from utilis import read_equations, parse_equation, append_variable, assign_inputs, combine_assigned_inputs, combine_outputs, startWrite, call_write
 
 # Change the working directory to the directory where your script is located
@@ -15,7 +14,7 @@ def main():
     choice = input("Enter 1 or 2: ")
     
     if choice == '1': 
-    
+        pass
     elif choice == '2': 
         filename = "test_examples/" + "fourInput.txt" 
         num_of_LUT, type_of_LUT, equations, file_name_without_extension, input_variables, output_variables = read_equations(filename)
@@ -27,23 +26,8 @@ def main():
         final_input = combine_outputs(assigned, output_list)
         final_input = final_output_expressions + final_input
         call_write(final_input, int(num_of_LUT[0]), file_name_without_extension)
-    
     else: 
         print("Invalid choice. Please enter '1' or '2'.")
-    
-
-if __name__ == "__main__":
-    main()
-    filename = "test_examples/" + "fourInput.txt" 
-    num_of_LUT, type_of_LUT, equations, file_name_without_extension, input_variables, output_variables = read_equations(filename)
-    startWrite(file_name_without_extension, input_variables, output_variables)
-    simplified = parse_equation(equations)
-    simplified, output_list= append_variable(simplified, equations, int(type_of_LUT[0]))
-    assigned = assign_inputs(simplified)
-    final_output_expressions = combine_assigned_inputs(assigned)
-    final_input = combine_outputs(assigned, output_list)
-    final_input = final_output_expressions + final_input
-    call_write(final_input, int(num_of_LUT[0]), file_name_without_extension)
     
 if __name__ == "__main__":
     main()
