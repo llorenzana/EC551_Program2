06ae606980f7f9fa5af8329806b6eb66c0ff893d
llorenzana <91199802+llorenzana@users.noreply.github.com>
2023-11-19 21:50:51 -0500

    fix equation parser

--- src/testCommands/equationParser.py
+++ src/testCommands/equationParser.py
@@ -1,10 +1,5 @@
-from sympy import symbols, sympify
-from sympy.logic.boolalg import to_dnf
-from itertools import product, combinations
 import os
-from writeToBlif import writeToBLIF
 
- 
 def read_equations(file_path):
     equations = []
     arbitrary_variable_counter = 0  # Counter for equations without an output variable
@@ -68,7 +63,7 @@ def generateMinterms(expression):
             minterms.append(binaryterm[i])
     minterms = list(set(minterms))
     
-    return simplifyExpression(minterms, variables)
+    return simplifyExpression([int(minterm, 2) for minterm in minterms], variables)
 
 def mintermToBinary(minterm, variables):
     binary = ''
@@ -102,22 +97,159 @@ def generateCombinations(binary, index=0, combinations=None):
 
     return combinations
 
-# takes teh minterms and simplifies them 
-def simplifyExpression (minterms, variables):
-    expanded_sum_of_minterms = []
+def simplifyExpression(mt, variables):
+    def mul(x,y): # Multiply 2 minterms
+        res = []
+        for i in x:
+            if i+"'" in y or (len(i)==2 and i[0] in y):
+                return []
+            else:
+                res.append(i)
+        for i in y:
+            if i not in res:
+                res.append(i)
+        return res
+
+    def multiply(x,y): # Multiply 2 expressions
+        res = []
+        for i in x:
+            for j in y:
+                tmp = mul(i,j)
+                res.append(tmp) if len(tmp) != 0 else None
+        return res
+
+    def refine(my_list,dc_list): # Removes don't care terms from a given list and returns refined list
+        res = []
+        for i in my_list:
+            if int(i) not in dc_list:
+                res.append(i)
+        return res
+
+    def findEPI(x): # Function to find essential prime implicants from prime implicants chart
+        res = []
+        for i in x:
+            if len(x[i]) == 1:
+                res.append(x[i][0]) if x[i][0] not in res else None
+        return res
+
+    def findVariables(pi): # Function to find variables in a meanterm. For example, the minterm --01 has C' and D as variables
+        # Map the binary representation in the prime implicant to the corresponding variables
+        result = []
+        for i, char in enumerate(pi):
+            if char == '1':
+                result.append(variables[i])
+            elif char == '0':
+                result.append(f'~{variables[i]}')
+        return result
+
+    def flatten(x): # Flattens a list
+        flattened_items = []
+        for i in x:
+            flattened_items.extend(x[i])
+        return flattened_items
+
+    def findminterms(a): #Function for finding out which minterms are merged. For example, 10-1 is obtained by merging 9(1001) and 11(1011)
+        gaps = a.count('-')
+        if gaps == 0:
+            return [str(int(a,2))]
+        x = [bin(i)[2:].zfill(gaps) for i in range(pow(2,gaps))]
+        temp = []
+        for i in range(pow(2,gaps)):
+            temp2,ind = a[:],-1
+            for j in x[0]:
+                if ind != -1:
+                    ind = ind+temp2[ind+1:].find('-')+1
+                else:
+                    ind = temp2[ind+1:].find('-')
+                temp2 = temp2[:ind]+j+temp2[ind+1:]
+            temp.append(str(int(temp2,2)))
+            x.pop(0)
+        return temp
+
+    def compare(a,b): # Function for checking if 2 minterms differ by 1 bit only
+        c = 0
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                mismatch_index = i
+                c += 1
+                if c>1:
+                    return (False,None)
+        return (True,mismatch_index)
+
+    def removeTerms(_chart,terms): # Removes minterms which are already covered from chart
+        for i in terms:
+            for j in findminterms(i):
+                try:
+                    del _chart[j]
+                except KeyError:
+                    pass
+                
+    dc = []
+    mt.sort()
+    minterms = mt
+    size = len(bin(minterms[-1]))-2
+    groups,all_pi = {},set()
+
+    # Primary grouping starts
     for minterm in minterms:
-        term = "("
-        for i, value in enumerate(minterm):
-            if value == '0':
-                term += f"~{variables[i]} & "
-            elif value == '1':
-                term += f"{variables[i]} & "
-        term = term[:-2]
-        expanded_sum_of_minterms.append(term)
-        
-    simplified = (") | ".join(expanded_sum_of_minterms) + ")")
-    simplified = str(to_dnf(simplified, simplify=True, force=True))
-    return simplified.replace("(", "").replace(")", "").replace("|", "+").replace("&", "").replace(" ", "")
+        try:
+            groups[bin(minterm).count('1')].append(bin(minterm)[2:].zfill(size))
+        except KeyError:
+            groups[bin(minterm).count('1')] = [bin(minterm)[2:].zfill(size)]
+    # Primary grouping ends
+
+
+    # Process for creating tables and finding prime implicants starts
+    while True:
+        tmp = groups.copy()
+        groups,m,marked,should_stop = {},0,set(),True
+        l = sorted(list(tmp.keys()))
+        for i in range(len(l)-1):
+            for j in tmp[l[i]]: # Loop which iterates through current group elements
+                for k in tmp[l[i+1]]: # Loop which iterates through next group elements
+                    res = compare(j,k) # Compare the minterms
+                    if res[0]: # If the minterms differ by 1 bit only
+                        try:
+                            groups[m].append(j[:res[1]]+'-'+j[res[1]+1:]) if j[:res[1]]+'-'+j[res[1]+1:] not in groups[m] else None # Put a '-' in the changing bit and add it to corresponding group
+                        except KeyError:
+                            groups[m] = [j[:res[1]]+'-'+j[res[1]+1:]] # If the group doesn't exist, create the group at first and then put a '-' in the changing bit and add it to the newly created group
+                        should_stop = False
+                        marked.add(j) # Mark element j
+                        marked.add(k) # Mark element k
+            m += 1
+        local_unmarked = set(flatten(tmp)).difference(marked) # Unmarked elements of each table
+        all_pi = all_pi.union(local_unmarked) # Adding Prime Implicants to global list
+        if should_stop: # If the minterms cannot be combined further
+            break
+    
+    # Process for creating tables and finding prime implicants ends
+
+
+    sz = len(str(mt[-1])) # The number of digits of the largest minterm
+    chart = {}
+    for i in all_pi:
+        merged_minterms,y = findminterms(i),0
+        for j in refine(merged_minterms,dc):
+            x = mt.index(int(j))*(sz+1) # The position where we should put 'X'
+            y = x+sz
+            try:
+                chart[j].append(i) if i not in chart[j] else None # Add minterm in chart
+            except KeyError:
+                chart[j] = [i]
+
+    EPI = findEPI(chart) # Finding essential prime implicants
+    removeTerms(chart,EPI) # Remove EPI related columns from chart
+
+    if(len(chart) == 0): # If no minterms remain after removing EPI related columns
+        final_result = [findVariables(i) for i in EPI] # Final result with only EPIs
+    else: # Else follow Petrick's method for further simplification
+        P = [[findVariables(j, ) for j in chart[i]] for i in chart]
+        while len(P)>1: # Keep multiplying until we get the SOP form of P
+            P[1] = multiply(P[0],P[1])
+            P.pop(0)
+        final_result = [min(P[0],key=len)] # Choosing the term with minimum variables from P
+        final_result.extend(findVariables(i) for i in EPI) # Adding the EPIs to final solution
+    return (' + ' .join(''.join(i) for i in final_result))
 
 # number of prime implicants & essential prime implicants
 def split_expression(expr, max_inputs):
@@ -245,14 +377,6 @@ def combine_assigned_inputs(assigned_inputs):
         formatted_expressions.append(formatted_expression)
     return formatted_expressions
 
-def call_write(final_input, num_LUTs, filename):
-    for i in range(len(final_input)):
-        if num_LUTs < len(final_input):
-            return "Not Possible"
-        else:
-            writeToBLIF(final_input[i], (filename + '.blif'))
-    return "blif write complete"
- 
 def main():
     filename = "src/test_examples/" + "fourInput.txt" 
     _, _, equations, file_name_without_extension, _, _ = read_equations(filename)
